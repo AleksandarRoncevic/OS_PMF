61cd9b98abc96fdbfef3532c1db3bebb7fe2b741
Aleksandar Roncevic <dmi.aleksandar.roncevic@student.pmf.uns.ac.rs>
2021-11-03 18:38:17 +0100

    Promaklo cuvanje cuttovanja teksta

--- vezbe1/Program.java
+++ vezbe1/Program.java
@@ -3,36 +3,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 
-/* Implementirati klasu 'Karta' sa osobinama 'boja' i 'rang' koje predstavljaju
- * standardne osobine karata klasicnog spila od 52+2 karte.
- * 
- * Potrebno je predstaviti sledece boje: pik, karo, herc i tref, dok su dozvo-
- * ljene vrednosti za rang poredjane po velicini: brojevi od 2 do 10, zandar,
- * kraljica, kralj i kec. Takodje je potrebno predstaviti i dva dzokera, jedan
- * u boji, jedan ne.
- * 
- * Implementirati klasu 'Spil' ciji konstruktor kreira nov spil koji sadrzi sve
- * 54 razlicite karte. Takodje, implementirati sledece operacije:
- * 
- *   int velicina()            - vraca broj karata trenutno u spilu
- *   Karta uzmiOdGore()        - ukljanja gornju kartu i vraca je kao rezultat
- *   Karta uzmiOdDole()        - ukljanja donju kartu i vraca je kao rezultat
- *   Karta uzmiIzSredine()     - ukljanja nasumicno izabranu kartu i vraca je
- *   void staviGore(Karta)     - dodaje kartu na vrh spila
- *   void staviDole(Karta)     - dodaje kartu na dno spila
- *   void staviUSredinu(Karta) - dodaje kartu na nasumicno izabrao mesto u spilu
- *   void promesaj()           - nasumicno rasporedjuje karte trenutno u spilu
- * 
- * Napisati program koji implementira sledecu igru za 12 igraca. Igraci redom
- * vuku po jednu kartu sa vrha spila i okrecu je. Program ispisuje koji igrac
- * je izvukao koju kartu. Pobednik je onaj igrac (ili igraci) cija je karta
- * najjaca, pri cemu se ne gleda boja karte a dzokeri su jaci od svih ostalih
- * karata. Ako je bilo vise pobednika igra se ponavlja samo sa pobednicima dok
- * ne ostane samo jedan. Program ispisuje ime konacnog pobednika.
- * 
- * Unapred smisliti imena za igrace, kreirati jedan spil i promesati ga pre
- * igre. Pretpostaviti da u toku igre nece nestati karata u spilu.
- */
 public class Program {
     private Spil spil;
     private String[] imena = 
